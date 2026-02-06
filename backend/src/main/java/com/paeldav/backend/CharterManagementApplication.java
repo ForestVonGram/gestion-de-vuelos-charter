784d7f1b24e6cc28b5e6c4dbbee4fe6a1e174754
David Gómez Ramírez <playa200612@gmail.com>
2026-02-06 12:15:12 -0500

    primer commit.

---
+++ backend/src/main/java/com/paeldav/backend/CharterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.paeldav.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharterManagementApplication.class, args);
+	}
+
+}
