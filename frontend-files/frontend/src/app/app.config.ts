68facecbc18486ff8f1e91a8547b31322195123a
David Gómez Ramírez <playa200612@gmail.com>
2026-02-14 19:16:11 -0500

    Carpetas base.

--- frontend-files/frontend/src/app/app.config.ts
+++ frontend-files/frontend/src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { RECAPTCHA_SETTINGS, RecaptchaSettings } from 'ng-recaptcha';
 
 import { routes } from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: RECAPTCHA_SETTINGS,
+      useValue: {
+        siteKey: ''
+      } as RecaptchaSettings,
+    }
   ]
 };
