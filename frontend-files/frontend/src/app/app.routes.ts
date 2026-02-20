561b1b8a7a78b847101ece283d2e81a8fa2ea0d9
David Gómez Ramírez <playa200612@gmail.com>
2026-02-20 15:02:07 -0500

    Creación del dashboard del admin.

--- frontend-files/frontend/src/app/app.routes.ts
+++ frontend-files/frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { ClientDashboardComponent } from './pages/cliente/dashboard_cliente/dashboard-cliente.component';
 import {authGuard} from './guards/auth.guard';
+import {adminGuard} from './guards/admin.guard';
+import {AdminDashboardComponent} from './pages/administrador/admin-dashboard/admin-dashboard.component';
 import {PreciosComponent} from './pages/precios/precios.component';
 
 export const routes: Routes = [
@@ -33,5 +35,15 @@ export const routes: Routes = [
     path: 'precios',
     component: PreciosComponent,
     canActivate: [authGuard]
-  }
+  },
+  {
+    path: 'admin',
+    canActivate: [adminGuard], // Esta línea bloquea a los que no son ADMIN
+    children: [
+      {
+        path: 'dashboard', // La ruta completa será: /admin/dashboard
+        component: AdminDashboardComponent
+      }
+    ]
+  },
 ];
