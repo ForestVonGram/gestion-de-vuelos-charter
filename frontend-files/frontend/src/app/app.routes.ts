9aad9ff8c114897e63f7d94860ae25d7a3d5b643
pabl0pez <pablo.lopez.g2005@gmail.com>
2026-02-21 17:30:07 -0500

    Preparacion para merge

--- frontend-files/frontend/src/app/app.routes.ts
+++ frontend-files/frontend/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { ClientDashboardComponent } from './pages/cliente/dashboard_cliente/dashboard-cliente.component';
+import { MantenimientoComponent } from './pages/operador/mantenimiento/mantenimiento.component';
+import { MantenimientoDetalleComponent } from './pages/operador/mantenimiento_detalle/mantenimiento_detalle.component';
+import { MantenimientoFormComponent } from './pages/operador/mantenimiento_form/mantenimiento_form.component';
 import {authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
