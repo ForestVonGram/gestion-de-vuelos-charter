8aa94b008300ca4b6616757236965a111dc50107
David Gómez Ramírez <playa200612@gmail.com>
2026-03-09 01:13:59 -0500

    Cambios.4

--- frontend-files/frontend/src/environments/environment.prod.ts
+++ frontend-files/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.astranimbus.com/api',
+  apiUrl: 'https://astranimbus-backend.onrender.com',
   recaptcha: {
     siteKey: '6LfIkmwsAAAAABHM4iYcWsi8Jdgz3c34ohTB0k7Q',
   }
