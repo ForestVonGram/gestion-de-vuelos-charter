7d2462a5b448c7cb825bad8b172b6d03fbac7747
David Gómez Ramírez <playa200612@gmail.com>
2026-03-09 01:36:32 -0500

    Cambios.6

--- frontend-files/frontend/src/environments/environment.prod.ts
+++ frontend-files/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://astranimbus-backend.onrender.com/api',
+  apiUrl: 'https://gestion-de-vuelos-charter.onrender.com/api',
   recaptcha: {
     siteKey: '6LfIkmwsAAAAABHM4iYcWsi8Jdgz3c34ohTB0k7Q',
   }
